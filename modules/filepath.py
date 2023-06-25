476878ebe523497c438e9bd36b0604fca5dda5c5
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2023-06-25 11:19:44 +0300

    feat(FilePath): create a function that sanitize the file path
    body: the function removes the leading and trailing whitespace and quotes

--- modules/filepath.py
+++ modules/filepath.py
@@ -1,12 +1,24 @@
 import os
 
-
 class FilePath:
+    @staticmethod
+    def sanitize_filepath(file_path):
+
+        file_path = file_path.strip("&")
+
+        # Remove leading and trailing whitespace
+        file_path = file_path.strip()
+
+        # Remove leading and trailing quotes
+        file_path = file_path.strip("'\"")
+
+        return file_path
 
     @staticmethod
     def read_filepath(message):
         while True:
             file_path = input(message)
+            file_path = FilePath.sanitize_filepath(file_path)
 
             # check if the file exists
             if os.path.exists(file_path):
