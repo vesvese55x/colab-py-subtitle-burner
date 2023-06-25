55077fabfb1ead43e17a99e8b35b72d214b16370
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2023-06-25 11:33:28 +0300

    feat: parse the srt file

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+import srt
 from modules.filepath import FilePath
 
 video_filepath = FilePath.read_video_filepath()
 srt_filepath = FilePath.read_srt_filepath()
+
+# read srt file contents
+with open(srt_filepath, "r", encoding="utf-8") as file:
+    srt_data = file.read()
+
+# parse srt
+parsed_subtitles = srt.parse(srt_data)
+
+#create list of subtitles
+subtitles_list = list(parsed_subtitles)
